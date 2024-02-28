ef1a637729c0e1a9a4835bab76c29545919d4071
rajesh <rajesh.t7982@gmail.com>
2024-02-28 09:18:09 +0530

    v3

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const exp=require('express')
-const app=exp();
-require('dotenv').config()
-const path=require('path')
+const exp = require("express");
+const app = exp();
+require("dotenv").config();
+const path = require("path");
 
 //API
 console.log("new line");
 
 
-
-const port=process.env.PORT;
-app.listen()
